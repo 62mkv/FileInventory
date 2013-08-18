3ae0c2e8fee386f44b6b8eb69cfcaf27769fc1fd
62mkv <62mkv@mail.ru>
2013-08-18 20:39:42 +0700

    First real commit

---
+++ wfFileInventory/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace wfFileInventory
+{
+    partial class fMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(fMain));
+            this.tbFolderPath = new System.Windows.Forms.TextBox();
+            this.bSelectFolder = new System.Windows.Forms.Button();
+            this.bStartScan = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.tvInventory = new System.Windows.Forms.TreeView();
+            this.SuspendLayout();
+            // 
+            // tbFolderPath
+            // 
+            resources.ApplyResources(this.tbFolderPath, "tbFolderPath");
+            this.tbFolderPath.Name = "tbFolderPath";
+            // 
+            // bSelectFolder
+            // 
+            resources.ApplyResources(this.bSelectFolder, "bSelectFolder");
+            this.bSelectFolder.Name = "bSelectFolder";
+            this.bSelectFolder.UseVisualStyleBackColor = true;
+            this.bSelectFolder.Click += new System.EventHandler(this.bSelectFolder_Click);
+            // 
+            // bStartScan
+            // 
+            resources.ApplyResources(this.bStartScan, "bStartScan");
+            this.bStartScan.Name = "bStartScan";
+            this.bStartScan.UseVisualStyleBackColor = true;
+            this.bStartScan.Click += new System.EventHandler(this.bStartScan_Click);
+            // 
+            // folderBrowserDialog1
+            // 
+            resources.ApplyResources(this.folderBrowserDialog1, "folderBrowserDialog1");
+            // 
+            // tvInventory
+            // 
+            resources.ApplyResources(this.tvInventory, "tvInventory");
+            this.tvInventory.Name = "tvInventory";
+            // 
+            // fMain
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.tvInventory);
+            this.Controls.Add(this.bStartScan);
+            this.Controls.Add(this.bSelectFolder);
+            this.Controls.Add(this.tbFolderPath);
+            this.Name = "fMain";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbFolderPath;
+        private System.Windows.Forms.Button bSelectFolder;
+        private System.Windows.Forms.Button bStartScan;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TreeView tvInventory;
+    }
+}
+
---
+++ wfFileInventory/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Resources;
+using System.Globalization;
+using System.Threading;
+
+namespace wfFileInventory
+{
+    public partial class fMain : Form
+    {
+        ResourceManager LocRM;
+        public fMain()
+        {
+            LocRM = new ResourceManager("wfFileInventory.wfResources", typeof(fMain).Assembly);
+            //Thread.CurrentThread.CurrentUICulture = new CultureInfo("ru-RU");
+            InitializeComponent();
+        }
+
+        private void bSelectFolder_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                tbFolderPath.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private void bStartScan_Click(object sender, EventArgs e)
+        {
+            /*TreeNode root=tvInventory.Nodes.Add("Root");
+            root.Nodes.Add("Child1");
+             */
+            try
+            {
+
+                DirectoryInfo di = new DirectoryInfo(tbFolderPath.Text);
+                if (di.Exists)
+                {
+                    TreeNode root = tvInventory.Nodes.Add(di.FullName);
+                    
+                    PopulateDirectoryBranch(tbFolderPath.Text, root);
+                }
+            }
+            catch (Exception E)
+            { MessageBox.Show(LocRM.GetString("Error_PathNotFound")+": ["+tbFolderPath.Text+"]\n"+E.Message); }
+        }
+
+        private void PopulateDirectoryBranch(string path, TreeNode start)
+        {
+            DirectoryInfo di = new DirectoryInfo(path);
+            IEnumerable<String> dirs = di.EnumerateDirectories().OrderBy(t => t.Name).Select(t => t.Name);
+            foreach (string dir in dirs)
+            {
+                TreeNode node = start.Nodes.Add(dir);
+                PopulateDirectoryBranch(path + "\\" + dir, node);
+            }
+        }
+
+    }
+}
